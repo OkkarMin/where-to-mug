64fd56b3eb294e429becc707f038b3b051b4ac0d
Okkar Min <omin001@e.ntu.edu.sg>
2021-06-24 20:01:17 +0800

    chore: :fire: Remove boilerplate code

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,21 +1,12 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { extendTheme } from "@chakra-ui/react";
 
-const fonts = { mono: `'Menlo', monospace` }
-
-const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-})
+const fonts = { mono: `'Menlo', monospace` };
 
 const theme = extendTheme({
   colors: {
-    black: '#16161D',
+    black: "#16161D",
   },
   fonts,
-  breakpoints,
-})
+});
 
-export default theme
+export default theme;
