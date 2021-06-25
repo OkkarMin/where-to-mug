3644f77b4feee23a40566d1b105c18a25c926716
Okkar Min <omin001@e.ntu.edu.sg>
2021-06-25 10:36:03 +0800

    feat: :sparkles: Add input search box for room name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,20 @@
-import { Container, Box, SimpleGrid } from "@chakra-ui/react";
+import {
+  Text,
+  Box,
+  Input,
+  HStack,
+  Flex,
+  Button,
+  SimpleGrid,
+  Menu,
+  MenuItem,
+  MenuButton,
+  MenuGroup,
+  MenuList,
+  Spacer,
+} from "@chakra-ui/react";
+import { ChevronDownIcon, TimeIcon } from "@chakra-ui/icons";
 
-import { BadgeTimeSlotsHeader } from "../components/BadgeTimeSlotsHeader";
 import { FreeRoomCard, IFreeRoomCard } from "../components/FreeRoomCard";
 
 const data = [
@@ -24,11 +38,63 @@ const data = [
   { location: "North Spine", time: "1530-1620", room: "TR+32" },
 ];
 
+const timeSlots = [
+  "0830-0930",
+  "0930-1030",
+  "1030-1130",
+  "1130-1230",
+  "1230-1330",
+  "1330-1430",
+  "1430-1530",
+  "1630-1730",
+  "1730-1830",
+  "1830-1930",
+  "1930-2030",
+];
+
 const Index = () => {
   return (
-    <Box w="full" h="auto" minH="100vh" bg="gray.100" mb="-4">
-      <BadgeTimeSlotsHeader />
-      <SimpleGrid minChildWidth="300px" spacing="4" mt="4" px={["4", "8"]}>
+    <Flex
+      direction="column"
+      w="full"
+      h="auto"
+      minH="100vh"
+      bg="gray.100"
+      mb="-4"
+    >
+      <HStack p={["4", "8"]}>
+        <Input
+          borderColor="#01A0DC"
+          placeholder="Enter room name"
+          colorScheme="linkedin"
+          variant="filled"
+        />
+
+        <Text>or...</Text>
+
+        <Menu placement="auto">
+          <MenuButton
+            as={Button}
+            colorScheme="linkedin"
+            size="md"
+            width="full"
+            leftIcon={<TimeIcon />}
+            rightIcon={<ChevronDownIcon />}
+          >
+            Timeslot
+          </MenuButton>
+          <MenuList>
+            <MenuGroup title="Timeslot">
+              {timeSlots.map((slot: string, i: number) => (
+                <MenuItem key={i} icon={<TimeIcon />}>
+                  {slot}
+                </MenuItem>
+              ))}
+            </MenuGroup>
+          </MenuList>
+        </Menu>
+      </HStack>
+      <SimpleGrid minChildWidth="300px" spacingX="4" mt="1" px={["4", "8"]}>
         {data.map((item: IFreeRoomCard, i: number) => (
           <FreeRoomCard
             key={i}
@@ -38,10 +104,11 @@ const Index = () => {
           ></FreeRoomCard>
         ))}
       </SimpleGrid>
+      <Spacer />
       <Box textAlign="center" fontSize="xs" py="4">
         2021 © Okkar, YingSheng, Raymond
       </Box>
-    </Box>
+    </Flex>
   );
 };
 
