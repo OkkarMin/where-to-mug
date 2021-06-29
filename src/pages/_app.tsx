87250e9125dd8a4cfaba64e0d48eca039935d531
Okkar Min <omin001@e.ntu.edu.sg>
2021-06-29 10:15:17 +0800

    feat: :mag: Add SEO related files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,37 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 
-import theme from '../theme'
-import { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <DefaultSeo
+        title="Which Room Free | NTU"
+        description="Ever wonder which tutorial rooms are 'free'? Which-room-free is here to help you determine which tutorial rooms are free and which are not 😊"
+        canonical="https://whichroomfree.ml"
+        openGraph={{
+          type: "website",
+          url: "https://whichroomfree.ml",
+          site_name: "Which Room Free",
+          title: "Which Room Free | NTU",
+          description:
+            "Ever wonder which tutorial rooms are 'free'? Which-room-free is here to help you determine which tutorial rooms are free and which are not 😊",
+          images: [
+            {
+              url: "/meta_image.png",
+              width: 1200,
+              height: 630,
+              alt: "Image of two students with a blue question mark in background",
+            },
+          ],
+        }}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
