a1a5c5b4917b7435bb08a9bc52ffce7ff62112ed
dannyyys <dannyyys@users.noreply.github.com>
2021-07-01 21:32:36 +0800

    feat: :sparkles: Add room not found image

--- src/components/FreeRoomsCardList.tsx
+++ src/components/FreeRoomsCardList.tsx
@@ -1,8 +1,9 @@
 import { FC } from "react";
 
-import { SimpleGrid } from "@chakra-ui/layout";
+import { SimpleGrid } from "@chakra-ui/react";
 
 import { FreeRoomCard } from "./FreeRoomCard";
+import { RoomNotFound } from "./RoomNotFound";
 
 import room_occupancy from "../../data/room_occupancy.json";
 import room_ids from "../../data/room_ids.json";
@@ -14,26 +15,26 @@ export const FreeRoomsCardList: FC<{
 }> = ({ searchText, timeSlot, stringDay }) => {
   const rooms = Object.keys(room_occupancy[stringDay]).sort();
 
-  return (
-    <SimpleGrid minChildWidth="340px" spacing="2" mt="4" px={["4", "8"]}>
-      {rooms
-        .filter((room: string) => {
-          const hasRoomName = room.includes(searchText.toUpperCase());
-          const hasAvailableSlot =
-            timeSlot == "All"
-              ? true
-              : room_occupancy[stringDay][room][timeSlot];
+  const fliteredRooms = rooms.filter((room: string) => {
+    const hasRoomName = room.includes(searchText.toUpperCase());
+    const hasAvailableSlot =
+      timeSlot == "All" ? true : room_occupancy[stringDay][room][timeSlot];
 
-          return hasRoomName && hasAvailableSlot;
-        })
-        .map((room: string, i: number) => (
-          <FreeRoomCard
-            key={i}
-            timeSlots={room_occupancy[stringDay][room]}
-            room={room}
-            room_id={room_ids[room]}
-          />
-        ))}
+    return hasRoomName && hasAvailableSlot;
+  });
+
+  return fliteredRooms.length > 0 ? (
+    <SimpleGrid minChildWidth="340px" spacing="2" mt="4" px={["4", "8"]}>
+      {fliteredRooms.map((room: string, i: number) => (
+        <FreeRoomCard
+          key={i}
+          timeSlots={room_occupancy[stringDay][room]}
+          room={room}
+          room_id={room_ids[room]}
+        />
+      ))}
     </SimpleGrid>
+  ) : (
+    <RoomNotFound />
   );
 };
---
+++ src/components/RoomNotFound.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+import { Box } from "@chakra-ui/react";
+
+export const RoomNotFound = () => {
+  return (
+    <Box align="center" width="auto">
+      <Box mt="10" boxSize="xs">
+        <Image
+          width="300"
+          height="200"
+          layout="responsive"
+          src="/assets/room_not_found_image.svg"
+          alt="Room not found image"
+        />
+      </Box>
+    </Box>
+  );
+};
