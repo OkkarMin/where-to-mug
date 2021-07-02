091fb614576224c3caa6f943ba8f8e01a00c4334
dannyyys <dannyyys@users.noreply.github.com>
2021-07-02 15:18:33 +0800

    test: :white_check_mark: Test server config

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 const dev = process.env.VERCEL_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "https://wheretomug.ml";
+export const server = dev
+  ? "https://where-to-mug-git-refactor-extractfiltertoapi-okkar.vercel.app"
+  : "https://wheretomug.ml";
