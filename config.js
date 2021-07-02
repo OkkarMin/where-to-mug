82f2aa08231f5bb6dc5ea4d3cde9273dbe7c7884
dannyyys <70012669+dannyyys@users.noreply.github.com>
2021-07-02 14:46:42 +0800

    Update env variable to vercel type

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.VERCEL_ENV !== "production";
 
 export const server = dev ? "http://localhost:3000" : "https://wheretomug.ml";
