77009cc2031baf130699329682fbdf4847dbc6b1
dannyyys <dannyyys@users.noreply.github.com>
2021-07-04 20:28:43 +0800

    chore: :fire: Remove unused codes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,6 @@
 import { Main } from "../components/Main";
 
-// const isWeekDay = (numberDay: number) => numberDay <= 5;
-
-// // const numberDay = new Date().getDay();
-// // !!! Remove this hardcoded line in production
-// const numberDay = 1;
-
 const Index = () => {
-  // return isWeekDay(numberDay) ? (
-  //   <Main numberDay={numberDay} />
-  // ) : (
-  //   <WeekendMessage />
-  // );
   return <Main />;
 };
 
