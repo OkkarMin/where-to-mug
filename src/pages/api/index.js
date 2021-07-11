a0dd288495736648ea2b21bb4921e5e9f5064e47
dannyyys <70012669+dannyyys@users.noreply.github.com>
2021-07-11 20:10:17 +0800

    feat: :sparkles: Add full text search (#33)
    * feat: :sparkles: Add full text search
    
    * Update src/pages/api/index.js
    
    Co-authored-by: Okkar Min <OMIN001@e.ntu.edu.sg>
    
    * refactor: :fire: Remove unuse codes
    
    * docs: :memo: Add threshold documentation
    
    Co-authored-by: dannyyys <dannyyys@users.noreply.github.com>
    Co-authored-by: Okkar Min <OMIN001@e.ntu.edu.sg>

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,21 +1,42 @@
+import Fuse from "fuse.js";
+
 import room_occupancy from "../../../data/room_occupancy.json";
 import cluster_data from "../../../data/rooms_with_cluster.json";
 
 export default function handler(req, res) {
-  const { query } = req;
-  const { currentDay, timeSlot, searchText, cluster } = query;
+  const { currentDay, timeSlot, searchText, cluster } = req.query;
 
   const rooms = Object.keys(room_occupancy[currentDay]).sort();
 
   const fliteredRooms = rooms.filter((room) => {
-    const hasRoomName = room.includes(searchText.toUpperCase());
     const hasAvailableSlot =
       timeSlot == "ALL" ? true : room_occupancy[currentDay][room][timeSlot];
     const isInTargetCluster =
       cluster == "ALL" ? true : cluster_data[room] == cluster;
 
-    return hasRoomName && hasAvailableSlot && isInTargetCluster;
+    return hasAvailableSlot && isInTargetCluster;
   });
 
-  return res.status(200).json(fliteredRooms);
+  const fuse = new Fuse(fliteredRooms, {
+    /**
+     * @see https://fusejs.io/api/options.html
+     * 0.35 is the most suitable value for our usecase after trail and error
+     * 0.35 returns relevant result for searchText "tr3", anything lower will return nothing
+     */
+    threshold: 0.35,
+  });
+
+  let matchingLocation = [];
+  let finalResult = [];
+
+  if (searchText == "") {
+    finalResult = fliteredRooms;
+  } else {
+    matchingLocation = fuse.search(searchText);
+    matchingLocation.map((object) => {
+      finalResult.push(object.item);
+    });
+  }
+
+  return res.status(200).json(finalResult);
 }
